5e8195bfc63f0ae5041534685df65ba1b3677e90
AtixCouz <kingphyr@outlook.com>
2024-09-08 12:52:36 +0200

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Playing VALORANT"];
+const statusMessages = ["Playing HRBRP","Watching All Criminals"];
 
 
 let currentIndex = 0;
